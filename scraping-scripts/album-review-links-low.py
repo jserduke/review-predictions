316867dac4edcbae8e5baa0158bb099a4b2df915
Jason Serduke <jason@serduke.com>
2026-05-08 21:13:38 -0700

    data collection files

---
+++ scraping-scripts/album-review-links-low.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import time
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+def fetch_html(url: str) -> str:
+    aoty_headers = {"User-Agent": "Mozilla/5.0 ()"}
+    req = Request(url, headers=aoty_headers)
+    with urlopen(req) as resp:
+        return resp.read().decode()
+
+def extract_album_links(html: str):
+    soup = BeautifulSoup(html, "html.parser")
+    album_links = soup.select(".albumListTitle a")
+    album_hrefs = []
+    for album_link in album_links:
+        album_href = album_link["href"]
+        album_hrefs.append(album_href)
+    return album_hrefs
+
+def main():
+    aoty_url = "https://www.albumoftheyear.org/ratings/6-highest-rated/2010s/"
+
+    for i in range(1, 200, 2):
+        html = fetch_html(aoty_url + str(i))
+        album_hrefs = extract_album_links(html)
+        with open("review-links-low.txt", "a") as review_links_file:
+            for album_href in album_hrefs:
+                review_links_file.write("https://www.albumoftheyear.org" + album_href[:-4] + "/user-reviews/?sort=worst\n")
+        print(i, "/200 done", sep="")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
---
+++ scraping-scripts/album-reviews-low.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""Simple web page fetch + title extraction skeleton (Beautiful Soup)."""
+
+import time
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+def fetch_html(url: str) -> str:
+    aoty_headers = {"User-Agent": "Mozilla/5.0 ()"}
+    req = Request(url, headers=aoty_headers)
+    with urlopen(req) as resp:
+        return resp.read().decode(errors="replace")
+
+def get_review_text(review_text_element):
+    review_paragraphs = review_text_element.select("p")
+    review_text = ""
+    for paragraph in review_paragraphs:
+        for break_tag in paragraph.select("br"):
+            break_tag.replace_with(" ")
+        review_text += paragraph.get_text() + " "
+    return review_text.strip()
+
+def extract_read_more_review(read_more_url: str):
+    read_more_html = fetch_html(read_more_url)
+    soup = BeautifulSoup(read_more_html, "html.parser")
+    reviewer_name = soup.select_one(".userName").get_text()
+    rating = soup.select_one(".albumCriticScore").get_text()
+    review_element = soup.select_one(".userReviewText")
+    review_text = get_review_text(review_element)
+    return (reviewer_name, rating, review_text)
+
+def extract_reviews(html: str):
+    soup = BeautifulSoup(html, "html.parser")
+    reviews = soup.select(".albumReviewRow")
+    review_texts = []
+    for review in reviews:
+        read_more_link = review.select_one("a.gray")
+        if read_more_link:
+            review_texts.append(extract_read_more_review("https://www.albumoftheyear.org" + read_more_link["href"]))
+            time.sleep(3)
+        else:
+            reviewer_name = review.select_one(".userReviewName").get_text()
+            rating = review.select_one(".rating").get_text()
+            review_element = review.select_one(".albumReviewText")
+            review_text = get_review_text(review_element)
+            review_texts.append((reviewer_name, rating, review_text))
+    return review_texts
+
+def main():
+    with open("review-links-low.txt") as review_links_file:
+        i = 0
+        for review_link in review_links_file:
+            current_review_link = review_link[:-1]
+            current_album_html = fetch_html(current_review_link)
+            time.sleep(3)
+            current_reviews = extract_reviews(current_album_html)
+            with open("reviews-low.txt", "a") as reviews_file:
+                for current_review in current_reviews:
+                    reviews_file.write(current_review[0] + "\t" + current_review[1] + "\t" + current_review[2] + "\n")
+            i += 1
+            print(i, "/2500 done", sep="")
+
+if __name__ == "__main__":
+    main()
